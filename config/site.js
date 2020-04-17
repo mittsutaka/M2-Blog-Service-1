0eca3e9f1fb28e2fd2858031cedafc67f14ce3a7
mittsutaka <mitsutaka.murao@tryeting.jp>
2020-04-17 23:03:47 +0900

    seo

---
+++ config/site.js
@@ -0,0 +1,17 @@
+module.exports = {
+    pathPrefix: '/',
+    title: 'm2-Gaming', // タイトル
+    titleAlt: 'My Wonderful Website', // JSONLDのためのタイトル
+    description: 'プログラミングに関するナレッジや趣味のゲームなど多岐にわたるプログラマの趣味ブログ',
+    url: 'https://m2-gaming.com', // スラッシュなしのサイトURL
+    siteURL: 'https://m2-gaming.com/', // スラッシュありのサイトURL
+    siteLanguage: 'ja', // HTMLの言語（ここでは日本語）
+    logo: 'src/images/gatsby-icon.png',
+    banner: 'src/images/gatsby-icon.png',
+    favicon: 'src/images/gatsby-icon.png', // ファビコン
+    shortName: 'm2Gaming', // サイトの略称、12文字以下
+    author: 'mitsutaka', // schemaORGJSONLDの作成者
+    themeColor: '#2E326A',
+    backgroundColor: '#F7F5F5',
+    twitter: '@pubglifesj', // TwitterのID
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,99 +1,95 @@
+const config = require('./config/site');
+
 module.exports = {
-  siteMetadata: {
-    title: `m2-Gaming`,
-    description: `自称プログラマによる趣味ブログ`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-109836197-3",
-        head: true,
-      }
+    siteMetadata: {
+        ...config
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `blogs`,
-        path: `${__dirname}/contents`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-emotion`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-plugin-graphql-codegen',
+    plugins: [{
+            resolve: "gatsby-plugin-google-analytics",
             options: {
-              fileName: `types/graphql-types.ts`,
-              documentPaths: [
-                "./src/**/*.{ts,tsx}",
-                // "./node_modules/gatsby-*/**/*.js"
-              ]
+                trackingId: "UA-109836197-3",
+                head: true,
             }
-          },
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
-            }
-          },
-          'gatsby-remark-prismjs-title',
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: 'language-',
-              inlineCodeMarker: null,
-              aliases: {
-                js: 'javascript',
-                sh: 'bash',
-              },
-              showLineNumbers: false,
-              noInlineHighlight: false,
-              languageExtensions: [
-                {
-                  language: 'superscript',
-                  extend: 'javascript',
-                  definition: { superscript_types: /(SuperType)/ },
-                  insertBefore: {
-                    function: { superscript_keywords: /(superif|superelse)/ },
-                  },
-                },
-              ],
-              prompt: { user: 'root', host: 'localhost', global: false },
-              escapeEntities: {},
+                name: `blogs`,
+                path: `${__dirname}/contents`,
             },
-          },
-        ]
-      }
-    }
-  ],
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-emotion`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [{
+                        resolve: 'gatsby-plugin-graphql-codegen',
+                        options: {
+                            fileName: `types/graphql-types.ts`,
+                            documentPaths: [
+                                "./src/**/*.{ts,tsx}",
+                                // "./node_modules/gatsby-*/**/*.js"
+                            ]
+                        }
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 590,
+                        }
+                    },
+                    'gatsby-remark-prismjs-title',
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: 'language-',
+                            inlineCodeMarker: null,
+                            aliases: {
+                                js: 'javascript',
+                                sh: 'bash',
+                            },
+                            showLineNumbers: false,
+                            noInlineHighlight: false,
+                            languageExtensions: [{
+                                language: 'superscript',
+                                extend: 'javascript',
+                                definition: { superscript_types: /(SuperType)/ },
+                                insertBefore: {
+                                    function: { superscript_keywords: /(superif|superelse)/ },
+                                },
+                            }, ],
+                            prompt: { user: 'root', host: 'localhost', global: false },
+                            escapeEntities: {},
+                        },
+                    },
+                ]
+            }
+        }
+    ],
 }
