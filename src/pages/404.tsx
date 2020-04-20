1d45554381f8e8ee671b8dfb1fde2a1f3367471d
mittsutaka <mitsutaka.murao@tryeting.jp>
2020-04-20 18:28:54 +0900

    修正

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -17,7 +17,7 @@ const NotFoundPage: React.FC<INotFoundProps> = ({ data }) => {
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
-    {/* </Layout> */ }
+    //</Layout>
   )
 }
 
