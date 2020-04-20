feeb3d51d612e634a464e8557fd380153657c22e
mittsutaka <mitsutaka.murao@tryeting.jp>
2020-04-20 18:05:14 +0900

    修正

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -15,7 +15,6 @@ const NotFoundPage:React.FC<INotFoundProps> = ({ data }) => {
 
   return (
     <Layout>
-      <SEO title="404: Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
