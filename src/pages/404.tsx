463665d8eac880a62cf0009adf2a8e211d4f0658
mittsutaka <mitsutaka.murao@tryeting.jp>
2020-04-20 18:34:08 +0900

    test

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,12 +12,10 @@ interface INotFoundProps {
 const NotFoundPage: React.FC<INotFoundProps> = ({ data }) => {
 
   return (
-    // <Layout>
-    <div>
+    <Layout>
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-    //</Layout>
+    </Layout>
   )
 }
 
